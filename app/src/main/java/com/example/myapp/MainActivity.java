4d70078f9e02145ba48f660ceb911588c3c98ace
LManuXx <71410341+LManuXx@users.noreply.github.com>
2022-03-03 13:47:26 +0100

    First commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.texto);
+    }
+}
